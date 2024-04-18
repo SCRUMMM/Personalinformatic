7e229dd4585b73e7922bd261db08cb3237ba487d
Henry <92936294+henry314159@users.noreply.github.com>
2024-04-18 20:24:50 +0100

    Update User.py

--- User.py
+++ User.py
@@ -1,16 +1,41 @@
 import hashlib
+import CSVReader
+import AlcoholConsumption
 
 class User:
     def __init__(self):
+        self.CSVReader = CSVReader.CSVReader()
+        self.AlcoholConsumption = None
+        self.userFile = None
+
+        self.loginOrRegister()
+        self.mainLoop()
+
+    def mainLoop(self):
+        # AlcoholConsumption records should be stored in a CSV (one per user) that
+        # User can use CSVReader to read from
+        # Or maybe AlcoholConsumption class could be serialised and stored in a normal text file?
+        pass
+
+    def enterNewAlcoholData(self):
+        pass
+
+    def loginOrRegister(self):
         self.credentialsFile = "credentials.txt"
         self.username = None
         x = ''
         while x not in ['l', 'r']:
-            x = input("Enter 'l' to login and 'r' to register a new account: ")
+            x = input("Enter 'l' to login or 'r' to register a new account: ")
         if x == 'l':
             self.login()
         else:
             self.register()
+
+    def getRawSleepData(self):
+        pass
+
+    def getRawAlcoholData(self):
+        pass
 
     def login(self):
         username = input("Enter your username: ")
@@ -22,6 +47,7 @@ class User:
             password = hashString(input("Enter your password: "))
             
         self.username = username
+        # should set self.userFile to the CSV file containing this users' data
 
     def check(self, username, password):
         with open(self.credentialsFile, 'r') as f:
@@ -46,6 +72,7 @@ class User:
             f.write(username+','+password+'\n')
 
         self.username = username
+        # should create new CSV file for users' data
 
 
 def hashString(string):
