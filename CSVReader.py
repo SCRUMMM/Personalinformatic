1fc1e4a888e7d42e23a78804f5134b5025f568fe
romurujouzu <89212227+romurujouzu@users.noreply.github.com>
2024-04-19 11:36:37 +0100

    Update CSVReader.py

--- CSVReader.py
+++ CSVReader.py
@@ -1,8 +1,9 @@
-"""CSVReader for CM12005 coursework by Borre Luijken 2024-04-15
+"""CSVReader for CM12005 coursework by Borre Luijken 2024-04-19
 
-Assumed CSV format: date, alcohol quantity, sleep hours, sleep quality
+Assumed sleep CSV format: date, sleep hours, sleep quality
+Assumed alcohol CSV format: date, alcohol quantity
 
-Reads the CSV and stores the data in 3 dictionaries:
+Reads the CSVs and stores the data in 3 dictionaries:
 1. alcohol  = {date : alcohol quantity}
 2. hours    = {date : sleep hours}
 3. quality  = {date : sleep quality}
@@ -11,7 +12,7 @@ Reads the CSV and stores the data in 3 dictionaries:
 ----- Example Usage -----
 
 reader = CSVReader()
-reader.read("C:/Documents/ExampleCSV")
+reader.read("C:/Documents/ExampleSleepCSV", "C:/Documents/ExampleAlcoholCSV")
 print(reader.quality['2024-04-15'])  # >>> "good"
 """
 
@@ -20,22 +21,22 @@ import csv
 
 
 class CSVReader:
-    
-    alchol = {}
-    hours = {}
-    quality = {}
-    
     def __init__(self):
         self.alcohol = {}
         self.hours = {}
         self.quality = {}
     
-    def read(self, filePath):
-        """Reads the CSV and puts the data in the dictionaries"""
-        with open(filePath, 'r') as f:
-            reader = csv.reader(f, delimiter=',')
+    
+    def read(self, filePathSleep, filePathAlcohol):
+        """Reads the CSVs and stores the data in the dictionaries"""
+        with open(filePathSleep, 'r') as fs:
+            reader = csv.reader(fs, delimiter=',')
+            for row in reader:
+                self.hours[row[0]] = row[1]
+                self.quality[row[0]] = row[2]
+        
+        with open(filePathAlcohol, 'r') as fa:
+            reader = csv.reader(fa, delimiter=',')
             for row in reader:
                 self.alcohol[row[0]] = row[1]
-                self.hours[row[0]] = row[2]
-                self.qualiy[row[0]] = row[3]
 
