2fbd10a8986d5d00fe58aff0794b0be147bdd1dd
Henry <92936294+henry314159@users.noreply.github.com>
2024-04-22 11:45:23 +0100

    Update sleep.py

--- sleep.py
+++ sleep.py
@@ -5,22 +5,22 @@ class sleep():
     
     def record(self, date, sleepdata):
         #sleepdata is stored as an array
-        self.consumption_record[date] = sleepdata
+        self.sleep_record[date] = sleepdata
 
     def get_quantity(self, date):
-        return self.consumption_record.get(date, 0)
+        return self.sleep_record.get(date, 0)
 
     def get_all_records(self):
-        return self.consumption_record
+        return self.sleep_record
 
     def set_sleepdata(self, date, sleepdata):
-        if date in self.consumption_record:
-            self.consumption_record[date] = sleepdata
+        if date in self.sleep_record:
+            self.sleep_record[date] = sleepdata
         else:
             print("No record found for the given date.")
 
     def delete_record(self, date):
-        if date in self.consumption_record:
-            del self.consumption_record[date]
+        if date in self.sleep_record:
+            del self.sleep_record[date]
         else:
             print("No record found for the given date.")
